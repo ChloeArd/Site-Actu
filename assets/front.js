73d59733a72d5087da2a715b0c1c99df2721e68f
ChloeArd <chloe.ardoise@gmail.com>
2021-11-19 16:10:01 +0100

    projet

--- assets/front.js
+++ assets/front.js
@@ -4,3 +4,20 @@ import {Button} from "./js/Button";
 let button = new Button();
 button.mode();
 button.add();
+
+/*const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+    }
+});
+
+xhr.open("GET", "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/TrendingNewsAPI?pageNumber=1&pageSize=10&withThumbnails=false&location=us");
+xhr.setRequestHeader("x-rapidapi-host", "contextualwebsearch-websearch-v1.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "295c2654b3mshfea991e404e656ep1a8222jsnd24204e876da");
+
+xhr.send(data);*/
--- assets/js/AddArticle.js
+++ assets/js/AddArticle.js
@@ -1,4 +1,6 @@
 
+/*créer un article a l'aide d'une api voir dans moodle*/
+
 export let tableArticle = [];
 
 export const AddArticle = function (author, title, image, content, date) {
@@ -13,17 +15,36 @@ export const AddArticle = function (author, title, image, content, date) {
         let container = document.getElementById("containerArticle");
         let x = tableArticle.length;
 
+        // a article
         let containerArt = document.createElement("div");
         containerArt.classList = "containerArt";
         document.getElementById("containerArticle").prepend(containerArt);
 
+        // contains the information
         let containerInfo = document.createElement("div");
         containerInfo.classList = "containerInfo";
         containerArt.append(containerInfo);
 
+        // create a author
+        let author = document.createElement("p");
+        author.innerHTML = this.author;
+        author.classList = "author";
+        containerInfo.append(author);
+
+        let title = document.createElement("h1");
+        title.innerHTML = this.title;
+        containerInfo.append(title);
+
+        // contains image
         let containerImage = document.createElement("div");
         containerImage.classList = "containerImage";
         containerArt.append(containerImage);
+
+        // create a image
+        let image = document.createElement("img");
+        image.src = this.image;
+        image.alt = this.title;
+        containerImage.append(image);
 
     };
 }
