464ce428998ee998eb041adc508d165f6f02ee52
ChloeArd <chloe.ardoise@gmail.com>
2021-11-22 16:46:49 +0100

    projet

--- assets/front.js
+++ assets/front.js
@@ -2,6 +2,26 @@ import "./css/front.css";
 import {Button} from "./js/Button";
 import {getCookie} from "./js/function";
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(function(reg) {
+
+        if(reg.installing) {
+            console.log('Service worker installing');
+        }
+        else if(reg.waiting) {
+            console.log('Service worker installed');
+        }
+        else if(reg.active) {
+            console.log('Service worker active');
+        }
+
+    }).catch(function(error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+    });
+}
+
+
 let dark_theme_class = 'dark-theme';
 let theme = getCookie('theme');
 
@@ -11,6 +31,3 @@ button.mode();
 button.home();
 button.articles();
 
-navigator.serviceWorker.register("../../service-worker.js").then(function () {
-    console.log("ok");
-})
--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
 self.addEventListener("install", function(event) {
     // Here we use waitUntil which waits for a promise to validate the installation
     event.waitUntil(
-        caches.open("jsmonthly").then(function(cache) {
+        caches.open("v1").then(function(cache) {
             // We cache a list of URLs that are the foundation of our app.
             return cache.addAll([
+                "/",
                 "/public/index.html",
                 "/public/build/css/front.css",
                 "/public/build/js/front.js"
@@ -14,23 +15,24 @@ self.addEventListener("install", function(event) {
 
 // We indicate that for each request, if it matches our cache URLs defined above, then we will serve the cache rather than making the real request over the network.
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.open("jsmontly").then(function(cache) {
-                    return cache.match(event.request);
-                }
-            )
-    );
-});
-
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-        navigator.serviceWorker.register("/service-worker.js").then(
-            function(registration) {
-                console.log("Registration succeeded. Scope is ", registration.scope);
-            },
-            function(err) {
-                console.log("Registration failed with ", err);
+    event.respondWith(caches.match(event.request).then(function(response) {
+            if (response !== undefined) {
+                return response;
             }
-        );
-    });
-}
+            else {
+                return fetch(event.request).then(function (response) {
+                    // response may be used only once
+                    // we need to save clone to put one copy in cache
+                    // and serve second one
+                    let responseClone = response.clone();
+
+                    caches.open('v1').then(function (cache) {
+                        cache.put(event.request, responseClone);
+                    });
+                    return response;
+                }).catch(function () {
+                    return caches.match('/public/build/js/front.js');
+                });
+            }
+    }));
+});
